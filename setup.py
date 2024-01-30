652196666a0d0c3823ba084004962316f1ce4abe
rcpsilva <rcpsilva@gmail.com>
2024-01-30 18:11:08 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name='DataCleaner',
     version='0.1',
     packages=find_packages(),
-    url='https://github.com/yourusername/your-repo',
+    url='https://github.com/rcpsilva/DataCleaning',
     license='MIT',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='Short description of your package',
+    author='Rodrigo Silva',
+    author_email='rcpsilva@gmail.com',
+    description='A simple data cleaner API for python',
     install_requires=[
-        # List your project's dependencies here
-        # e.g., 'pandas>=1.0',
+        'pandas >= 2.1',
+        'scikit-learn >= 1.3',
     ],
+    python_requires='>=3.6'
 )

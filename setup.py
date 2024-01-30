2bc70967b09ce9427a48c94b3863733ec439050e
rcpsilva <rcpsilva@gmail.com>
2024-01-30 19:40:03 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DataCleaner',
+    name='DataCleaning',
     version='0.1',
     packages=find_packages(),
     url='https://github.com/rcpsilva/DataCleaning',
